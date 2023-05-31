b4f4d863400a5de67ca50117245366726555fa4a
LaNice Powell <lanice.powell@newsela.com>
2023-05-30 20:48:39 -0400

    spice

--- dataStructures-demo 7/script.js
+++ dataStructures-demo 7/script.js
@@ -20,3 +20,21 @@ console.log(myArray)
 let lastElement = myArray.pop()
 console.log('lastElement:', lastElement)
 console.log('updated:', myArray)
+
+
+// splice(x,y,z ) => Allow us to target a specific index that we would liketo remove from an array. 
+// x will target by the index
+// y will speciify how many items after that index we would like to remove. 
+// z will add in another item in that place
+
+
+let candy= ['kitkat', 'Mars','reeses','skittles', 'sneakers' ]
+console.log('candy', candy.length)
+// targets the idex of 1 and removes one item which is Mars
+let removeMars = candy.splice(1, 1)
+console.log('removeMars', removeMars)
+console.log('newCandyList', candy)
+// target the index of 1, removes no items and adds marshmellow
+let addMarshmellow= candy.splice(1, 0, 'marshemellow') 
+
+console.log('candy2 ', candy)
