b1b7ad27d0b55e1c2d8e71191b215a6a50b25251
LaNice Powell <lanice.powell@newsela.com>
2023-05-30 21:16:36 -0400

    adding objects

--- dataStructures-demo 7/script.js
+++ dataStructures-demo 7/script.js
@@ -12,3 +12,24 @@
  * Arrays
  */
 
+// OBJECTS
+let person = {
+  name: "John Doe",
+  age : 75 , 
+  height : "5'11", 
+
+}
+// targets using dot notation
+console.log('person', person);
+console.log('personAge', person.age)
+
+//Create and empty object
+let soonFullObject = {}
+soonFullObject["name"] = 'Was Empty'
+console.log(soonFullObject)
+soonFullObject.age = 2
+console.log(soonFullObject)
+soonFullObject.id = 678
+console.log(soonFullObject)
+
+
